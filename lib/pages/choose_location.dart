764ee811ac9c3f8a83d7e292f9c5e780977ccca2
clickingmouse <clickingmouse@users.noreply.github.com>
2019-12-23 09:38:33 +0800

    testing async/await

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,10 +6,52 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+  void getData()async{
+    // simulate network request for a usrename
+
+    String username = await Future.delayed(Duration(seconds:3), (){
+      //print('yoshi');
+      return 'yoshi';
+    });
+    print('statement');
+
+    //simulate network req to get bio of username
+    Future.delayed(Duration(seconds:2), (){
+      print('vega, musician, egg collector');
+    });
+  }
+
+
+
+  int counter=0;
+  @override
+    void initState() {
+
+      super.initState();
+      print('init state function ran');
+    }
+
   @override
   Widget build(BuildContext context) {
+    print('build function ran');
+getData();
     return Scaffold(
-      body:Text("Choose location screen")
+      backgroundColor:Colors.grey[200],
+      appBar:AppBar(
+        backgroundColor:Colors.blue[900],
+        title: Text('Choose a location'),
+          centerTitle:true,
+        elevation:0,
+      ),
+      body:RaisedButton(
+        onPressed:(){
+          setState((){
+            counter +=1;
+          });
+        },
+        child:Text('counter is $counter')
+      )
     );
   }
 }
