6f74cce560c372d7e4406720943c22ba97d1343d
clickingmouse <clickingmouse@users.noreply.github.com>
2019-12-23 10:58:47 +0800

    separate logic into package

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,15 +8,27 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData()async{
+//  void getData()async{
+//
+//  Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+//  Map data = jsonDecode(response.body);
+//  print(data);
+//  print(data['title']);
+//
+//  }
 
-  Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
-  Map data = jsonDecode(response.body);
-  print(data);
-  print(data['title']);
 
-  }
+String time = 'loading...';
 
+void setupWorldTime() async {
+  WorldTime instance = WorldTime(location:'Berlin', flag:'germany.png', url:'Europe/Berlin');
+ await instance.getTime();
+ //print(instance.time);
+ setState((){
+   time = instance.time;
+ });
+
+}
 
 
 
@@ -25,13 +36,19 @@ class _LoadingState extends State<Loading> {
   void initState() {
 
     super.initState();
-    getData();
+//    getData();
+//  getTime();
+  setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Text('loading screen'),
+//      body:Text('loading screen'),
+    body:Padding(
+      padding:EdgeInsets.all(50.0),
+          child:Text(time),
+    ),
     );
   }
 }
